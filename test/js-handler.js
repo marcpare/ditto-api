87e27ff0f680cc5c09ed2cd55341bbb070fac961
Marc Pare <marcpare0@gmail.com>
2015-03-22 01:29:22 -0700

    js handler test

---
+++ test/js-handler.js
@@ -0,0 +1,23 @@
+var test = require('tape');
+var DittoFactory = require('./ditto-factory');
+var request = require('request');
+
+var ditto = DittoFactory({
+  config: 'configs/js-handler/ditto.json'
+});
+ditto.start();
+
+test('js handler successful login', function (t) { 
+  request({
+    method: 'post',
+    url: ditto.baseUrl+'token',
+    json: {
+      email: 'test@example.com',
+      password: 'password'
+    }
+  }, function (err, resp, body) {
+    t.deepEqual(body, {"token": "helloworldtoken"});
+    ditto.stop();
+    t.end();
+  });
+});
