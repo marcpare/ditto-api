d4deddc041a479d6b2c0d184c0a9892424d41782
Marc Pare <marcpare0@gmail.com>
2015-03-22 00:14:20 -0700

    Load and create static routes

--- ditto.js
+++ ditto.js
@@ -6,16 +6,60 @@ var path = require('path');
 function Ditto (options) {
   options = options || {};
   if (!options.port) throw "Missing options.port";
+  if (!options.config) throw "Missing config file location";
+  
+  this.config = JSON.parse(fs.readFileSync(options.config));
+  this.baseDir = path.dirname(options.config);
 }
+
+Ditto.prototype.relativeJSON = function (path) {
+  return JSON.parse(fs.readFileSync(path.join(this.baseDir, path)));
+};
+
+Ditto.prototype.addRoute = function (route) {
+  var ditto = this;
+  
+  // LATER: factor out the `addRoute` methods
+  
+  routeAdders = [
+    {
+      detect: function (route) {
+        return route.response && /\.json$/.test(route.response);
+      },
+      addRoute: function (route) {
+        var jsonPath = route.response;
+        delete route.response;        
+        route.handler = function (request, reply) {
+          return ditto.relativeJSON(jsonPath);
+        }
+        ditto.server.route(route);
+      }
+    }
+  ];
+  
+  // Add the route using the first matching adder
+  for (var i = 0; i < routeAdders.length; i++) {
+    if (routeAdders[i].detect(route)) {
+      routeAdders[i].addRoute(route);
+      break;
+    }
+  }
+    
+};
+
+Ditto.prototype.processConfig = function () {
+  var routes = this.config.routes || [];
+  routes.forEach(this.addRoute, this);
+};
 
 Ditto.prototype.start = function () {
   var server = new Hapi.Server();
-  
+  this.server = server;  
   server.connection({
     host: 'localhost',
     port: this.port
   });
-  
+  this.processConfig();
   server.start();
 };
 
--- test/run.js
+++ test/run.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var Ditto = require('../ditto');
 var ditto = new Ditto({
-  port: 34345
+  port: 34345,
+  config: 'example-configs/static/ditto.json'
 });
 ditto.start();
