546d2409fb17a66b46a7800f52fbe4bf1448a105
Marc Pare <marcpare0@gmail.com>
2015-03-22 19:41:03 -0700

    Pull the route adders out into separate objects

--- ditto.js
+++ ditto.js
@@ -40,54 +40,56 @@ Ditto.prototype.jsonReply = function (request, reply, jsonPath) {
   reply(this.relativeJSON(resolvedPath));
 };
 
+var staticRouteAdder = {
+  detect: function (route) {
+    return route.response && /\.json$/.test(route.response);
+  },
+  addRoute: function (ditto, route) {
+    var jsonPath = route.response;
+    delete route.response;
+    route.handler = function (request, reply) {
+      ditto.jsonReply(request, reply, jsonPath);          
+    }
+    ditto.server.route(route);
+  }
+};
+
+var jsRouteAdder = {
+  detect: function (route) {
+    return route.handler;
+  },
+  addRoute: function (ditto, route) {
+    var customHandler = route.handler;
+    route.handler = function (request, reply) {
+      // Custom context to provide helpers to the handler so that
+      // you can do things like:
+      //     
+      //    this.jsonReply('token.json')
+      // 
+      var handlerContext = {
+        jsonReply: function (jsonPath, options) {
+          ditto.jsonReply(request, reply, jsonPath);
+        }
+      };
+      customHandler = customHandler.bind(handlerContext);
+      return customHandler(request, reply);
+    };
+    ditto.server.route(route);
+  }
+};
+
 Ditto.prototype.route = function (route) {
   var ditto = this;
   
-  // LATER: factor out the `addRoute` methods
-  
   routeAdders = [
-    {
-      detect: function (route) {
-        return route.response && /\.json$/.test(route.response);
-      },
-      addRoute: function (route) {
-        var jsonPath = route.response;
-        delete route.response;
-        route.handler = function (request, reply) {
-          ditto.jsonReply(request, reply, jsonPath);          
-        }
-        ditto.server.route(route);
-      }
-    },
-    {
-      detect: function (route) {
-        return route.handler;
-      },
-      addRoute: function (route) {
-        var customHandler = route.handler;
-        route.handler = function (request, reply) {
-          // Custom context to provide helpers to the handler so that
-          // you can do things like:
-          //     
-          //    this.jsonReply('token.json')
-          // 
-          var handlerContext = {
-            jsonReply: function (jsonPath, options) {
-              ditto.jsonReply(request, reply, jsonPath);
-            }
-          };
-          customHandler = customHandler.bind(handlerContext);
-          return customHandler(request, reply);
-        };
-        ditto.server.route(route);
-      }
-    }
+    staticRouteAdder,
+    jsRouteAdder
   ];
   
   // Add the route using the first matching adder
   for (var i = 0; i < routeAdders.length; i++) {
     if (routeAdders[i].detect(route)) {
-      routeAdders[i].addRoute(route);
+      routeAdders[i].addRoute(ditto, route);
       break;
     }
   }
